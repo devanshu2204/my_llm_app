6206f5efd459ff4112bbaf4e4ae525d04398cff7
devanshu2204 <133222298+devanshu2204@users.noreply.github.com>
2025-05-16 18:43:39 +0530

    Your message about local changes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "60544270"
+    serviceWorkerVersion: "4218323650"
   }
 });
